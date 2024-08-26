a558cdacb0d159a67ceba66ce0ff1e27696e317e
sambitsks <sahu83253@gmail.com>
2024-08-26 11:18:20 +0530

    first commit

---
+++ mi/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def captain(request):
+    return HttpResponse('ro is a best betsman')
+
+
+def captain(request):
+    return HttpResponse('rohit is mind blowing')
